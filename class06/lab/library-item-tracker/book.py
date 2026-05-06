e3cdc57234c93b6e59090e7e0821cbf136ab47b4
TrongTamDao <daotrongtam80@gmail.com>
2026-05-06 10:46:11 -0400

    Class attritubes, Class and static methods

--- class06/lab/library-item-tracker/book.py
+++ class06/lab/library-item-tracker/book.py
@@ -1,14 +1,21 @@
 class Book:
+    
+    library_name = "Central Library"
+    
+    @staticmethod
+    def is_valid_title(title):
+        if(len(title.strip())>0): return True
+    
     def __init__(self, title, author, available):
         self.title = title
         self.author = author
-        self.available = True
+        self.available = available
         
     def display_book(self):
         print(f"title: {self.title} - author: {self.author} - avaibility: {self.available}")
+    
+    
+    @classmethod
+    def change_library_name(cls, name):
+        cls.library_name = name
         
-b1 = Book("Harry Potter and Philosopher's stone", "J. K. Rowling", True)
-b2 = Book("Justice, what's the right thing to do?", "Micheal J.Sandel",  True)
-
-b1.display_book()
-b2.display_book()
---
+++ class06/lab/library-item-tracker/main.py
@@ -0,0 +1,11 @@
+from book import Book
+
+b1 = Book("Harry Potter and Philosopher's stone", "J. K. Rowling", True)
+b2 = Book("Justice, what's the right thing to do?", "Micheal J.Sandel",  True)
+
+b1.display_book()
+b2.display_book()
+
+Book.change_library_name("West wing")
+print(Book.library_name)
+print(Book.is_valid_title(b1.title))
