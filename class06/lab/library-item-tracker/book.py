e06a14f2de5b178a2f65a239d12ea826ffbf156a
TrongTamDao <daotrongtam80@gmail.com>
2026-05-06 12:08:34 -0400

    adding genre

--- class06/lab/library-item-tracker/book.py
+++ class06/lab/library-item-tracker/book.py
@@ -1,19 +1,23 @@
 class Book:
     
     library_name = "Central Library"
+    count = 0
     
     @staticmethod
     def is_valid_title(title):
         if(len(title.strip())>0): return True
     
-    def __init__(self, title, author, available):
+    def __init__(self, title, author, genre, available):
         self.title = title
         self.author = author
         self.available = available
+        self.genre = self.genre
+        Book.count += 1
         
     def display_info(self):
         print(f"title: {self.title}")  
         print(f"author: {self.author}")
+        print(f"genre: {self.genre}")
         print(f"availability: {self.available}")
         
     def borrow(self):
@@ -27,10 +31,20 @@ class Book:
             self.available = True
             print(f"{self.title} has been returned.")
         else:
-            print(f"{self.title} was returned")
+            print(f"{self.title} is already returned")
     
     
     @classmethod
     def change_library_name(cls, name):
         cls.library_name = name
+        
+    @classmethod
+    def show_count(cls):
+        print(f"total book: {cls.count}")
+        
+    @classmethod
+    def from_string(cls, data):
+        title, author, available = data.split(",")
+        return cls(title, author, available)
+        
         
--- class06/lab/library-item-tracker/main.py
+++ class06/lab/library-item-tracker/main.py
@@ -5,7 +5,16 @@ b2 = Book("Justice, what's the right thing to do?", "Micheal J.Sandel",  True)
 
 b1.display_info()
 b2.display_info()
+print("---------------------------------")
 
 Book.change_library_name("West wing")
 print(Book.library_name)
 print(Book.is_valid_title(b1.title))
+print("-------------------------------------")
+
+Book.show_count()
+b3 = Book.from_string("Clean Code, Robert C. Martin, True")
+Book.show_count()
+b3.display_info()
+print("-------------------------------------")
+
