9d5587c85032a9846ad86058217388b0e2b2d4b4
TrongTamDao <daotrongtam80@gmail.com>
2026-06-17 07:20:26 -0400

    added utils.py

--- class17/movie_booking/main.py
+++ class17/movie_booking/main.py
@@ -4,14 +4,19 @@ from core.constants import MAX_TICKETS_PER_BOOKING
 from models.movie_show import MovieShow
 from models.customer import Customer
 from models.staff import Staff
+from utils import *
 
 def main():
     customer = Customer("Ava")
-    show = MovieShow("Inception", 20, ShowStatus.OPEN)
-
+    show = MovieShow(format_title("Inception"), 20, ShowStatus.OPEN)
+    
+    print_separator()
     customer.display_info()
+    print_separator()
     show.display_info()
+    print_separator()
     print("Max tickets per booking:", MAX_TICKETS_PER_BOOKING)
+    print_separator()
 
 if __name__ == "__main__":
     main()
---
+++ class17/movie_booking/utils.py
@@ -0,0 +1,5 @@
+def format_title(title):
+    return title.upper()
+
+def print_separator():
+    print ("-" * 40)
